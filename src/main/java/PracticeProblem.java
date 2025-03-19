910bf35b912aeb3bb70151a644fce7af046adb5b
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-03-19 14:11:44 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,32 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int sum2D (int[][] array2D) {
+		int sum = 0;
+		for (int row = 0; row < array2D.length; row++){
+			for (int col = 0; col < array2D[row].length; col++){
+				sum = sum + array2D[row][col];
+				}
+			}
+		return sum;
+		
+		}
+
+
+	public static int sumRow (int[][] array2D, int row){
+		int sum = 0;
+		for(int i = 0; i < array2D[row].length; i++){
+			sum = sum + array2D[row][i];
+		}
+		return sum;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static int sumColumn (int[][] array2D, int col){
+		int sum = 0;
+		for(int j = 0; j < array2D.length; j++){
+				sum = sum + array2D[j][col];
+		}
+		return sum;
 	}
 
 }
